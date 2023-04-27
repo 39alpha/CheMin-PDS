467dcabf88e45afa915482ee2460cb4edfaa89f5
Douglas G. Moore <doug@39alpharesearch.org>
2023-04-26 21:23:25 -0700

    Setup gov.nasa.pds.validate

--- src/main/java/org/thirtyninealpharesearch/pds/PDS3To4.java
+++ src/main/java/org/thirtyninealpharesearch/pds/PDS3To4.java
@@ -1,9 +1,17 @@
 package org.thirtyninealpharesearch.pds;
 
+import gov.nasa.pds.validate.ValidateLauncher;
+
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
+
 import java.util.ArrayList;
+
+import org.apache.commons.io.IOUtils;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -15,13 +23,17 @@ public class PDS3To4
 {
     public static void main(String[] args)
     {
+        System.setProperty("java.util.logging.config.file", "logging.properties");
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "ERROR");
+        System.setProperty("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "false");
+
         String labelFilename = args[0];
 
         String templateFilename = "org/thirtyninealpharesearch/pds/example.vm";
         if (args.length >= 2) {
             templateFilename = args[1];
         } else {
-            Velocity.setProperty(RuntimeConstants.RESOURCE_LOADERS, "classpath"); 
+            Velocity.setProperty(RuntimeConstants.RESOURCE_LOADERS, "classpath");
             Velocity.setProperty("resource.loader.classpath.class", ClasspathResourceLoader.class.getName());
             Velocity.init();
         }
@@ -34,39 +46,42 @@ public class PDS3To4
         list.add("Tucker");
         context.put("list", list);
 
-        Label label = null;
         try {
-            label = LabelBuilder.parseFile(labelFilename);
-        } catch (IOException e) {
-            System.err.printf("Error Reading %s\n", labelFilename);
-            System.exit(1);
-        }
+            Label label = LabelBuilder.parseFile(labelFilename);
+            System.out.printf("PDS Version: %s\n", label.PDSVersionId);
 
-        Template template = null;
-        try {
-            template = Velocity.getTemplate(templateFilename);
-        } catch (Exception e) {
-            System.err.printf("Error Reading %s\n", templateFilename);
-            System.exit(1);
-        }
-
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        template.merge(context, writer);
-
-        try {
+            Template template = Velocity.getTemplate(templateFilename);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+            template.merge(context, writer);
             writer.flush();
-        } catch (Exception e) {
-            System.err.println("Failed to flush template");
-            System.exit(1);
-        }
 
-        try {
+            File registeredProductsFile = PDS3To4.getResourceAsFile("/util/registered_context_products.json");
+            ValidateLauncher v = new ValidateLauncher();
+            v.setRegisteredProductsFile(registeredProductsFile);
+            v.processMain(new String[]{"temp.xml"});
+
             writer.close();
         } catch (Exception e) {
             System.err.println("Failed to close writer");
             System.exit(1);
         }
+    }
 
-        System.out.printf("PDS Version: %s", label.PDSVersionId);
+    public static File getResourceAsFile(String resourcePath) {
+        try {
+            InputStream in = PDS3To4.class.getResourceAsStream(resourcePath);
+            if (in == null) {
+                return null;
+            }
+
+            File tempFile = File.createTempFile(String.valueOf(in.hashCode()), ".tmp");
+            tempFile.deleteOnExit();
+
+            IOUtils.copy(in, new FileOutputStream(tempFile));
+            return tempFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
