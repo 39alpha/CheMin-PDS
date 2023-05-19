d0765af9ea4c0c57658da803b8c162578933e96d
Douglas G. Moore <doug@39alpharesearch.org>
2023-05-19 15:28:34 -0700

    Use picocli to parse command line options

--- src/main/java/org/thirtyninealpharesearch/chemin/PDS3To4.java
+++ src/main/java/org/thirtyninealpharesearch/chemin/PDS3To4.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -13,14 +19,19 @@ import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import org.thirtyninealpharesearch.chemin.pds3.RDR4Label;
 
-public class PDS3To4 {
-    private int run(String[] args) throws Exception {
-        String labelFilename = args[0];
+@Command(name="pds3to4", mixinStandardHelpOptions=true, version="1.0-alpha",
+         description="Convert PDS3 label files to PDS4")
+public class PDS3To4 implements Callable<Integer> {
+    @Parameters(index="0", paramLabel="PDS3-LABEL", description="path to label file")
+    private String labelFilename;
 
-        String templateFilename = "org/thirtyninealpharesearch/pds4/RDR4.vm";
-        if (args.length >= 2) {
-            templateFilename = args[1];
-        } else {
+    @Option(names={"-t", "--template"}, paramLabel="TEMPLATE", description="path to template file")
+    private String templateFilename = null;
+
+    @Override
+    public Integer call() throws Exception {
+        if (templateFilename == null) {
+            templateFilename = "org/thirtyninealpharesearch/pds4/RDR4.vm";
             Velocity.setProperty(RuntimeConstants.RESOURCE_LOADERS, "classpath");
             Velocity.setProperty("resource.loader.classpath.class", ClasspathResourceLoader.class.getName());
             Velocity.init();
@@ -44,18 +55,13 @@ public class PDS3To4 {
         return exitCode;
     }
 
-    public static void main(String[] args)
+    public static void main(String... args)
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "ERROR");
         System.setProperty("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "false");
 
-        try {
-            int exitCode = new PDS3To4().run(args);
-            System.exit(exitCode);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
+        int exitCode = new CommandLine(new PDS3To4()).execute(args);
+        System.exit(exitCode);
     }
 }
