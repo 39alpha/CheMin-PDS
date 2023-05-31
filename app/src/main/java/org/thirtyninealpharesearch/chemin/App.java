25e5217d21c2b6e9f3fc031e9411c0cf5fd523f7
Douglas G. Moore <doug@39alpharesearch.org>
2023-05-31 14:27:07 -0700

    Break up App.call for testing

--- app/src/main/java/org/thirtyninealpharesearch/chemin/App.java
+++ app/src/main/java/org/thirtyninealpharesearch/chemin/App.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
@@ -20,6 +21,7 @@ import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import org.thirtyninealpharesearch.chemin.pds3.Label;
+import org.thirtyninealpharesearch.chemin.pds3.Label.LabelType;
 
 @Command(name="pds3to4", mixinStandardHelpOptions=true, version="1.0-alpha",
          description="Convert PDS3 label files to PDS4")
@@ -38,6 +40,37 @@ public class App implements Callable<Integer> {
 
     @Option(names={"-l", "--label-type"}, paramLabel="TYPE", description="label type (rda, re1 or min)")
     private String labelType = null;
+
+    public static void run(Label label, String templateFilename, Writer writer) throws Exception {
+        VelocityContext context = new VelocityContext();
+        context.put("label", label);
+
+        Template template = Velocity.getTemplate(templateFilename);
+        template.merge(context, writer);
+    }
+
+    public static void run(Label label, LabelType labelType, Writer writer) throws Exception {
+        if (labelType == LabelType.UNKNOWN) {
+            labelType = label.inferLabelType();
+        }
+
+        String templateFilename = null;
+        switch(labelType) {
+            case RDA:
+                templateFilename = "org/thirtyninealpharesearch/chemin/pds4/RDA.vm";
+                break;
+            case RE1:
+                templateFilename = "org/thirtyninealpharesearch/chemin/pds4/RE1.vm";
+                break;
+            case MIN:
+                templateFilename = "org/thirtyninealpharesearch/chemin/pds4/MIN.vm";
+                break;
+            case UNKNOWN:
+                throw new Exception("cannot infer label type from file; please provider --label-type argument");
+        }
+
+        App.run(label, templateFilename, writer);
+    }
 
     @Override
     public Integer call() throws Exception {
@@ -60,49 +93,23 @@ public class App implements Callable<Integer> {
             System.exit(1);
         }
 
-        if (templateFilename == null) {
-            Label.LabelType labelType = Label.LabelType.UNKNOWN;
-            try {
-                labelType = label.inferLabelType();
-            } catch (Exception e) {
-                System.err.println("error: " + e.getMessage());
-                CommandLine.usage(this, System.err);
-                System.exit(1);
-            }
-            if (labelType == Label.LabelType.UNKNOWN) {
-                labelType = label.inferLabelType();
-            }
-            switch(labelType) {
-                case RDA:
-                    templateFilename = "org/thirtyninealpharesearch/chemin/pds4/RDA.vm";
-                    break;
-                case RE1:
-                    templateFilename = "org/thirtyninealpharesearch/chemin/pds4/RE1.vm";
-                    break;
-                case MIN:
-                    templateFilename = "org/thirtyninealpharesearch/chemin/pds4/MIN.vm";
-                    break;
-                case UNKNOWN:
-                    System.err.println("error: cannot infer label type from file; please provider --label-type argument");
-                    CommandLine.usage(this, System.err);
-                    System.exit(1);
-            }
-        }
-
-        VelocityContext context = new VelocityContext();
-        context.put("label", label);
-
-        Template template = Velocity.getTemplate(templateFilename);
         File pds4File = new File(outputFilename);
         BufferedWriter writer = new BufferedWriter(new FileWriter(pds4File));
-        template.merge(context, writer);
-        writer.flush();
 
-        int exitCode = new Validator().process(new String[]{outputFilename});
+        try {
+            if (templateFilename != null) {
+                App.run(label, templateFilename, writer);
+            } else {
+                App.run(label, this.getLabelType(), writer);
+            }
+            writer.flush();
+            writer.close();
 
-        writer.close();
-
-        return exitCode;
+            return new Validator().process(new String[]{outputFilename});
+        } catch (Exception e) {
+            System.err.println("error: " + e.getMessage());
+            return 1;
+        }
     }
 
     protected Label.LabelType getLabelType() throws Exception {
